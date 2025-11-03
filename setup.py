b0e4dbc58826390c248ee29de2528a47f3ce4d03
Purushotham Chintha <puruchintha@gmail.com>
2025-11-03 19:55:51 +0530

    changes

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ def get_requirements(file_path: str) -> List[str]:
 setup(
     name='mlproject',
     version='0.0.1',
-    author='Krish',
-    author_email='krishnaik06@gmail.com',
+    author='Puruchintha',
+    author_email='puruchintha@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
 
