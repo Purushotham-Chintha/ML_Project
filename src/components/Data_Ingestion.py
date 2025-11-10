82cd0a1b73eea4272c3dbc35611760f1063f39c9
Purushotham Chintha <puruchintha@gmail.com>
2025-11-10 18:22:21 +0530

    application

--- src/components/Data_Ingestion.py
+++ src/components/Data_Ingestion.py
@@ -6,6 +6,11 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.logger import logging
 from src.exception import CustomException
+from src.components.Data_transformation import DataTransformation
+from src.components.Data_transformation import DataTransformationConfig
+
+from src.components.Model_Trainer import ModelTrainerConfig
+from src.components.Model_Trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -46,3 +51,11 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    # modeltrainer = ModelTrainer()
+    # print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+    print(train_arr)
